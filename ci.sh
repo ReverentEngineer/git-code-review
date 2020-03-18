7e639aa749497e2cb83c4b40f4f08f12b8606e9d
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-03-17 20:08:37 -0400

    Update CI workflow

---
+++ ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ -z ${GITHUB_ACTOR} ]]; then
+  echo "No GITHUB_ACTOR defined."
+  exit 1;
+fi
+
+if [[ -z ${GITHUB_TOKEN} ]]; then
+  echo "No GITHUB_TOKEN defined."
+  exit 1;
+fi
+
+URL=https://$(GITHUB_ACTOR):$(GITHUB_TOKEN)@github.com/ReverentEngineer/git-code-review 
+
+git config --global user.name "Github Action"
+git config --global user.mail "jeff@reverentengineer.com"
+git clone --branch=gh-pages $URL docs/build/html
+cd docs/build/html && git rm -rf ./*
+make -C docs html
+cd docs/build/html && git add ./* && git commit -m "Updates" && git push
